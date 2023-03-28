c30e94ae7d3d1e9a9d74233be35df769e7f27182
Flavio Andrade <foa12381@gmail.com>
2023-03-28 11:42:44 -0300

    poo

---
+++ poo/poo.js
@@ -0,0 +1,10 @@
+const person = {
+  nome: 'John Smith',
+  age: 34,
+  cpf: "1928384756",
+  email: 'john@smith.com'
+}
+
+console.log(person)
+console.log(person.cpf.substring(0,3))
+console.log(person['email'])
